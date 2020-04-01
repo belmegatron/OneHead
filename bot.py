3ca7040c0af0c625237a696900bd2267c4235baf
Rich <richardbelmega@gmail.com>
2020-04-01 01:36:51 +0100

    Removed join and leave commands. Added register/signup commands.

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import os
 import time
 from dotenv import load_dotenv
 from discord.ext import commands
-import discord
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,16 +12,20 @@ class OneHead(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.game_in_progress = False
+        self.is_balanced = False
+        self.discord_uid_steamid_map = {}
+        self.signups = []
 
-    @commands.command()
-    async def join(self, ctx):
-        channel = ctx.author.voice.channel
-        await channel.connect()
-        await ctx.send("1Head IGC IHL Bot reporting for duty sir...")
+        self.radiant = []
+        self.dire = []
 
-    @commands.command()
-    async def leave(self, ctx):
-        await ctx.voice_client.disconnect()
+    def reset_state(self):
+
+        self.game_in_progress = False
+        self.is_balanced = False
+        self.signups = []
+        self.radiant = []
+        self.dire = []
 
     @commands.command()
     async def start(self, ctx):
@@ -30,6 +33,9 @@ class OneHead(commands.Cog):
         if self.game_in_progress:
             await ctx.send("Game already in progress...")
             return
+
+        if self.is_balanced is False:
+            await self.balance(ctx)
 
         await ctx.send("Game starting in ...")
         for second in range(3, 0, -1):
@@ -42,13 +48,14 @@ class OneHead(commands.Cog):
     async def stop(self, ctx):
         if self.game_in_progress:
             await ctx.send("Game stopped.")
-            self.game_in_progress = False
+            self.reset_state()
         else:
             await ctx.send("No currently active game.")
 
-    @commands.command()
     async def balance(self, ctx):
         await ctx.send("Balancing teams...")
+        # TODO: Calculate how to balance teams based on DB entries.
+        self.is_balanced = True
 
     @commands.command()
     async def result(self, ctx, side):
@@ -62,11 +69,57 @@ class OneHead(commands.Cog):
             return
 
         await ctx.send("{} Victory!".format(side))
+        # TODO: Update DB
         await ctx.send("Updating Scores...")
+        self.reset_state()
 
     @commands.command()
     async def leaderboard(self, ctx):
         await ctx.send("Current IGC Leaderboard:")
+
+    @commands.command()
+    async def register(self, ctx, steam_id):
+
+        #   TODO: Sanity check steam_id.
+
+        if not steam_id:
+            await ctx.send("Please supply 64bit SteamID.")
+            return
+
+        if ctx.author.display_name not in self.discord_uid_steamid_map.keys():
+            self.discord_uid_steamid_map[ctx.author.display_name] = steam_id
+            await ctx.send("Successfully Registered.")
+        else:
+            await ctx.send("Already registered.")
+
+    @commands.command()
+    async def deregister(self, ctx):
+
+        if ctx.author.display_name in self.discord_uid_steamid_map.keys():
+            self.discord_uid_steamid_map.pop(ctx.author.display_name)
+            await ctx.send("Successfully Deregistered.")
+        else:
+            await ctx.send("Discord Name could not be found.")
+
+    @commands.command()
+    async def signup(self, ctx):
+        if ctx.author.display_name in self.signups:
+            await ctx.send("{} is already signed up.".format(ctx.author.display_name))
+        elif len(self.signups) >= 10:
+            ctx.send("Signups full.")
+        else:
+            self.signups.append(ctx.author.display_name)
+
+        await ctx.send("Current Signups: {}".format(self.signups))
+
+    @commands.command()
+    async def signout(self, ctx):
+        if ctx.author.display_name not in self.signups:
+            await ctx.send("{} is not currently signed up.".format(ctx.author.display_name))
+        else:
+            self.signups.remove(ctx.author.display_name)
+
+        await ctx.send("Current Signups: {}".format(self.signups))
 
 
 bot.add_cog(OneHead(bot))
