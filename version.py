0d115aeb0697de82f369312f2c1caf6aed90177b
Rich <richardbelmega@gmail.com>
2020-04-28 00:56:41 +0100

    No longer teardown channels. Check if channels exist, if not we create them. Adjusted unit tests.

--- version.py
+++ version.py
@@ -1,6 +1,4 @@
-__version__ = 1.11
+__version__ = 1.12
 __changelog__ = "```" \
-                  "* Picks and Nominations during a CM game are now case insensitive.\n" \
-                  "* Time to pick during a CM game has been increased from 20s to 30s.\n" \
-                  "* Patch note summary now available when using the !version command." \
-                  "```"
+                "* IHL Channels are no longer deleted upon stopping a game." \
+                "```"
