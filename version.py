2c9262e8127ef4cfa089ef70b842e780417404ea
Rich <richardbelmega@gmail.com>
2020-04-29 18:31:01 +0100

    Added more verbose changelog message.

--- version.py
+++ version.py
@@ -1,4 +1,5 @@
 __version__ = 1.14
 __changelog__ = "```" \
-                "* If signups exceed 10, then players will be selected at random." \
+                "* Now handles the case where there are more than 10 players signed up. If this happens then " \
+                "10 players will be selected at random and the remaining players benched." \
                 "```"
