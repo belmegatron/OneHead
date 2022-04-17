d39cfca26e42652588fcb2f0eaa3a94a7280c182
Rich <richardbelmega@gmail.com>
2022-04-17 16:20:54 +0100

    Added script for updating db.

---
+++ util/configure_db_fields.py
@@ -0,0 +1,24 @@
+import decimal
+
+from onehead.common import OneHeadCommon
+from onehead.db import OneHeadDB
+from pprint import pprint
+
+if __name__ == "__main__":
+
+    config = OneHeadCommon.load_config()
+    database = OneHeadDB(config)
+
+    scoreboard = database.retrieve_table()
+    players = [x["name"] for x in scoreboard]
+
+    for player in players:
+        database.db.update_item(
+            Key={"name": player},
+            UpdateExpression="set steam_id = :zero",
+            ExpressionAttributeValues={":zero": decimal.Decimal(0)},
+            ReturnValues="UPDATED_NEW",
+        )
+
+    scoreboard = database.retrieve_table()
+    pprint(scoreboard)
